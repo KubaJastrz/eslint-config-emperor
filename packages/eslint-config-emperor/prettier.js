bd4f40af152f214206b6eb1282941b16f2527dc3
Jakub Jastrzębski <jakub.jastrzebski@pm.me>
2023-04-02 20:54:48 +0200

    chore: fix link

--- packages/eslint-config-emperor/prettier.js
+++ packages/eslint-config-emperor/prettier.js
@@ -1,4 +1,4 @@
-// See https://github.com/microsoft/rushstack/blob/main/eslint/eslint-config/README.md
+// See https://github.com/microsoft/rushstack/blob/main/eslint/eslint-patch/README.md
 require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
